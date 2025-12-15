fa4b18bf12cea2962ef6adee81258393980cbfe3
FrankFungcode <combeebe@gmail.com>
2025-12-15 23:38:12 +0800

    feat: 添加log4js日志显示，如404和500

--- app.ts
+++ app.ts
@@ -11,12 +11,22 @@ import config from "@config/index";
 
 // views\index.html 中 {{ data }}  服务端渲染
 import render from "@koa/ejs"; // 后端模板引擎EJS koa/ejs
-     
+
+import ErrorHandler from "@middlewares/ErrorHandler";
+
 import { createContainer, Lifetime } from "awilix";
 import { loadControllers, scopePerRequest } from "awilix-koa";
 import Koa from "koa";
 
+import { configure, getLogger } from "log4js";
+
 const app = new Koa();
+
+//日志系统
+configure({
+  appenders: { cheese: { type: "file", filename: `${__dirname}/logs/yd.log` } },
+  categories: { default: { appenders: ["cheese"], level: "error" } },
+});
 
 // ** 1. 创建容器 来自 awilix
 const container = createContainer();
@@ -45,7 +55,13 @@ render(app, {
   debug: false,
 });
 
+// 在路由生效前
+const logger = getLogger("cheese");
+ErrorHandler.error(app, logger);
+
 // awilix 会自动的分析 构造函数需要哪些 service，如ApiController 中的，然后从容器中取出来，注入进去
+
+//让所有的路由全部生效
 // ** 3. 把所有的路由全部load进来
 app.use(loadControllers(`${__dirname}/routers/*{.ts,.js}`));
 
