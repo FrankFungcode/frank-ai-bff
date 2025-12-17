a1d57381a81c34f8d6bcb423c75d2c77139344fc
FrankFungcode <combeebe@gmail.com>
2025-12-17 14:35:00 +0800

    feat: mpa + spa

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
  * @Author: FrankFungcode combeebe@gmail.com
  * @Date: 2025-12-16 00:32:24
  * @LastEditors: FrankFungcode combeebe@gmail.com
- * @LastEditTime: 2025-12-17 14:18:45
+ * @LastEditTime: 2025-12-17 14:26:39
  * @FilePath: \frank-ai-bff\app.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -84,11 +84,11 @@ ErrorHandler.error(app, logger);
 app.use(loadControllers(`${__dirname}/routers/*{.ts,.js}`));
 
 
-// if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'development') {
   // ECS EC2 本地运行 listen 端口 8081
   app.listen(port || 8081, () => {
     console.log('server is running at http://localhost:8081');
   });
-// }
+}
 
 export default app;
